0f6b83db1be7de0b86c437ead3673d092644caa7
AzeOcram <mrodon@up.edu.ph>
2026-03-21 13:03:09 +0800

    feat: added politicians-section

---
+++ js/politicians-data.js
@@ -0,0 +1,20 @@
+const politiciansData = [
+  {
+    id: 1,
+    firstname: "BONG BONG",
+    lastname: "MARCOS",
+    photo: "../images/politician.png",
+    position: "President",
+    party: "PFP",
+    successRate: "64%"
+  },
+  {
+    id: 2,
+    firstname: "JIRO",
+    lastname: "TIPAN",
+    photo: "https://placehold.co/400x600/cccccc/555555?text=Photo",
+    position: "Senator",
+    party: "NPC",
+    successRate: "45%"
+  }
+];
