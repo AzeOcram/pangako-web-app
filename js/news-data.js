01ffdbcd2689a4116a346a5485d19fbb98223153
AzeOcram <mrodon@up.edu.ph>
2026-03-21 12:47:04 +0800

    feat: added detailed news section

---
+++ js/news-data.js
@@ -0,0 +1,93 @@
+const newsData = [
+  {
+    id: 1,
+    title: "NEWS HEADLINE INSERT HERE ADSLFJNDS ASDFJESLFC DMAKL",
+    date: "March 2026",
+    image: "https://placehold.co/1200x500/cccccc/555555?text=News+Photo",
+    body: [
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+    ]
+  },
+  {
+    id: 2,
+    title: "NEWS HEADLINE INSERT HERE ADSLFJNDS ASDFJESLFC DMAKL",
+    date: "February 2026",
+    image: "https://placehold.co/1200x500/cccccc/555555?text=News+Photo",
+    body: [
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    ]
+  },
+  {
+    id: 3,
+    title: "NEWS HEADLINE INSERT HERE ADSLFJNDS ASDFJESLFC DMAKL",
+    date: "January 2026",
+    image: "https://placehold.co/1200x500/cccccc/555555?text=News+Photo",
+    body: [
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    ]
+  },
+  {
+    id: 4,
+    title: "NEWS HEADLINE INSERT HERE ADSLFJNDS ASDFJESLFC DMAKL",
+    date: "January 2026",
+    image: "https://placehold.co/1200x500/cccccc/555555?text=News+Photo",
+    body: [
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    ]
+  },
+  {
+    id: 5,
+    title: "NEWS HEADLINE INSERT HERE ADSLFJNDS ASDFJESLFC DMAKL",
+    date: "December 2025",
+    image: "https://placehold.co/1200x500/cccccc/555555?text=News+Photo",
+    body: [
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    ]
+  },
+  {
+    id: 6,
+    title: "NEWS HEADLINE INSERT HERE ADSLFJNDS ASDFJESLFC DMAKL",
+    date: "December 2025",
+    image: "https://placehold.co/1200x500/cccccc/555555?text=News+Photo",
+    body: [
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    ]
+  },
+  {
+    id: 7,
+    title: "NEWS HEADLINE INSERT HERE ADSLFJNDS ASDFJESLFC DMAKL",
+    date: "November 2025",
+    image: "https://placehold.co/1200x500/cccccc/555555?text=News+Photo",
+    body: [
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    ]
+  },
+  {
+    id: 8,
+    title: "NEWS HEADLINE INSERT HERE ADSLFJNDS ASDFJESLFC DMAKL",
+    date: "November 2025",
+    image: "https://placehold.co/1200x500/cccccc/555555?text=News+Photo",
+    body: [
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    ]
+  },
+  {
+    id: 9,
+    title: "NEWS HEADLINE INSERT HERE ADSLFJNDS ASDFJESLFC DMAKL",
+    date: "October 2025",
+    image: "https://placehold.co/1200x500/cccccc/555555?text=News+Photo",
+    body: [
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    ]
+  }
+];
